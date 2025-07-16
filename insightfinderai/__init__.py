bc3a706af3c6126437dc74406bfbbc04390aab30
Mustafa M <neyaadeez@gmail.com>
2025-07-16 18:02:52 -0400

    updated

--- insightfinderai/__init__.py
+++ insightfinderai/__init__.py
@@ -5,10 +5,8 @@ A user-friendly Python SDK for the InsightFinder AI platform.
 Designed for easy use with beautiful, formatted outputs.
 """
 
-from .client import Client, ChatResponse, EvaluationResult
+from .client import Client
+from .model import ChatResponse, EvaluationResult, BatchEvaluationResult, BatchChatResult
 
 __version__ = "2.0.0"
-__all__ = ["Client", "ChatResponse", "EvaluationResult"]
-
-__version__ = "0.1.0"
-__all__ = ['Client', '__version__']
+__all__ = ["Client", "ChatResponse", "EvaluationResult", "BatchEvaluationResult", "BatchChatResult"]
